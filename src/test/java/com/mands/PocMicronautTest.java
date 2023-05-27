345dcfc9339b45763cc764c5eafdaa4377a8c70a
Somasundaram Maharajan <somasundaram.maharajan@marks-and-spencer.com>
2023-05-27 15:18:07 +0000

    Initial commit

---
+++ src/test/java/com/mands/PocMicronautTest.java
@@ -0,0 +1,21 @@
+package com.mands;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class PocMicronautTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
